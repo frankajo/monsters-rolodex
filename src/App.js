74a3d21e28dd15fc92a4919e26f5f913e6cac803
unknown <ajongbede.frank@gmail.com>
2024-04-03 12:58:49 +0100

    Sample Commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ const App = () => {
   const [monsters, setMonsters] = useState([]);
   const [filtteredMonsters, setFiltteredMonsters] = useState(monsters);
 
+  console.log('render')
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
